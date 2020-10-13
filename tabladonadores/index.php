e4df0e239e08d7afc86aec9027832958711e5299
Adrián Torres Hernández <A01173530@itesm.mx>
2020-10-13 12:36:52 -0500

    Actualizacion

--- tabladonadores/index.php
+++ tabladonadores/index.php
@@ -1,13 +1,8 @@
 <?php 
 
-<<<<<<< HEAD
-	<!-- CSS Alerttify -->
-	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
-	<link rel="stylesheet" type="text/css" href="componentestabla/styles.css"> 
-=======
+
   include ("_nav.html"); 
   include("_tabladonadores.html");
->>>>>>> Capas
 
 
 
