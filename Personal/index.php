3f4ec979c78c05533ff010e0113e256622eff089
Adrián Torres Hernández <A01173530@itesm.mx>
2020-10-19 22:32:46 -0500

    Actualizacion

---
+++ Personal/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+  require_once("php/model.php");
+
+
+  session_start();
+
+  include("_header.html");
+  include("_container.html");
+  include("_modal_registrar.html");
+
+  include("_tabla_personal.html");
+  include("_boton_registrar.html");
+
+  include("_endcontainer.html");
+  include("_footer.html"); 
+
+ ?>
