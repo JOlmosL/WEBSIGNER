0d41f5bdbf07493488d27391bd0d8cb52e354959
Adrián Torres Hernández <A01173530@itesm.mx>
2020-11-12 23:37:45 -0600

    Archivos

--- Personal/php/model.php
+++ Personal/php/model.php
@@ -63,7 +63,7 @@ function tabla_personal( $criterio= "" ) {
         $resultado .= '<td>'.$row["CorreoPersonal"].'</td>';
         $resultado .= '<td>'.$row["FechaInicioLaboral"].'</td>';
         $resultado .= '<td>'.$row["FechaFinLaboral"].'</td>';
-        $resultado .= '<td><a href= "'.$row["IdPersonal"].'" target= "_blank">Documentos</a></td>';
+        $resultado .= '<td><a href="subirArchivos.php?id='.$row["IdPersonal"].'">Documentos</a></td>';
        /*$resultado .= '<td><a href= "'.$row["ContratoPersonal"].'" target= "_blank">Contrato</a></td>';
         $resultado .= '<td><a href= "'.$row["INEPersonal"].'" target= "_blank">INE</a></td>';
         $resultado .= '<td><a href= "'.$row["DomicilioPersonal"].'" target= "_blank">Domicilio</a></td>';
@@ -182,93 +182,8 @@ function actualizar_personal($id, $nombre, $telefono, $correo, $fechai, $fechaf 
 
 //actualizar_personal('10','CMLL','9678103', 'poke@hotmail.com', '11/11/20', '12/11/21');
 
-function verificar_traidor($acusado_id) {
-    $resultado = "No es un traidor.";
-    $conexion_bd = conectar();
-    
-    $consulta = "SELECT nombre
-                 FROM tripulante
-                 WHERE traidor = 1 AND id = ".$acusado_id;
-    
-    $resultados_consulta = $conexion_bd->query($consulta); 
-    
-    while ($row = mysqli_fetch_array($resultados_consulta, MYSQLI_ASSOC)) {
-        
-        $resultado = "Descubriste al traidor de ".$row["nombre"]; 
-    }
-    
-    mysqli_free_result($resultados_consulta); //Liberar la memoria
-    
-    desconectar($conexion_bd);
-    return $resultado;
-}
 
-function set_impostor($id) {
-    $conexion_bd = conectar();
-    
-    $consulta_todos_inocentes = "UPDATE tripulante SET traidor=0";
-    
-    $conexion_bd->query($consulta_todos_inocentes); 
-    
-    $consulta_nuevo_impostor = "UPDATE tripulante SET traidor=1 WHERE id=?";
-    
-    if(!($statement = $conexion_bd->prepare($consulta_nuevo_impostor))) {
-        die("Error(".$conexion_bd->errno."): ".$conexion_bd->error);
-    }
-    
-    if(!($statement->bind_param("s",$id))) {
-        die("Error de vinculación(".$statement->errno."): ".$statement->error);
-    }
-    
-    if(!$statement->execute()) {
-        die("Error en ejecución de la consulta(".$statement->errno."): ".$statement->error);
-    }
-    
-    desconectar($conexion_bd);
-}
 
-function get_nombre($id) {
-    $resultado = "";
-    $conexion_bd = conectar();
-    
-    $consulta = "SELECT nombre
-                 FROM tripulante
-                 WHERE id = ".$id;
-    
-    $resultados_consulta = $conexion_bd->query($consulta); 
-    
-    while ($row = mysqli_fetch_array($resultados_consulta, MYSQLI_ASSOC)) {
-        
-        $resultado = $row["nombre"]; 
-    }
-    
-    mysqli_free_result($resultados_consulta); //Liberar la memoria
-    
-    desconectar($conexion_bd);
-    return $resultado;
-}
-
-function set_tripulante($id, $nombre) {
-    $conexion_bd = conectar();
-    
-    $consulta = "UPDATE tripulante SET nombre=? WHERE id=?";
-    
-    if(!($statement = $conexion_bd->prepare($consulta))) {
-        die("Error(".$conexion_bd->errno."): ".$conexion_bd->error);
-    }
-    
-    if(!($statement->bind_param("ss",$nombre, $id))) {
-        die("Error de vinculación(".$statement->errno."): ".$statement->error);
-    }
-    
-    if(!$statement->execute()) {
-        die("Error en ejecución de la consulta(".$statement->errno."): ".$statement->error);
-    }
-    
-    desconectar($conexion_bd);
-    
-    $_SESSION["info"] = "Se actualizó el tripulante $id";
-}
 
 function tabla_archivo( $criterio= "" ) {
     
@@ -285,7 +200,7 @@ function tabla_archivo( $criterio= "" ) {
     $resultados_consulta = $conexion_bd->query($consulta);  
  //   var_dump($consulta);
     $resultado = '<table id="personal" class="table table-hover table-condensed table-bordered">';
-    $resultado .= '<thead class="bg-warning"><tr><th>Nombre completo</th><th>Teléfono</th><th>Correo electrónico</th><th>Inicio de colaboración</th><th>Fin de colaboración</th><th>Contrato</th><th>INE</th><th>Domicilio</th><th>Respaldo</th><th>Editar</th><tr></thead>';
+    $resultado .= '<thead class="bg-warning"><tr><th>Referencia del Archivo</th><th>Nombre del archivo</th><th>Subido</th><th>Descargar</th><tr></thead>';
     
     while ($row = mysqli_fetch_array($resultados_consulta, MYSQLI_ASSOC)) { 
         //$resultado .= '<td>'.$row["IdPersonal"].'</td>';
@@ -294,12 +209,7 @@ function tabla_archivo( $criterio= "" ) {
         $resultado .= '<td>'.$row["CorreoPersonal"].'</td>';
         $resultado .= '<td>'.$row["FechaInicioLaboral"].'</td>';
         $resultado .= '<td>'.$row["FechaFinLaboral"].'</td>';
-        $resultado .= '<td><a href= "'.$row["ContratoPersonal"].'" target= "_blank">Contrato</a></td>';
-        $resultado .= '<td><a href= "'.$row["INEPersonal"].'" target= "_blank">INE</a></td>';
-        $resultado .= '<td><a href= "'.$row["DomicilioPersonal"].'" target= "_blank">Domicilio</a></td>';
-        $resultado .= '<td> <a href= "'.$row["RespaldoPersonal"].'" target= "_blank">Respaldo</a></td>';
-     
-        $resultado .= '<td>'. '<a class="btn btn-secondary" href="editarPersonal.php?id='.$row["IdPersonal"].'"><svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg></a>'.'</td>';
+        $resultado .= '<td>'. '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg>'.'</td>';
 
         $resultado .= '</tr>';
     }
