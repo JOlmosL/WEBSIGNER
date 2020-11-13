aa2b4b22a5f74728a664ef8f4829e6562d2e219b
Royd117 <A01273078@itesm.mx>
2020-11-13 13:54:15 -0600

    Dinamic Tables Productos

---
+++ Productos/ConsultaP.php
@@ -0,0 +1,4 @@
+<?php
+	$TodosProductos = "SELECT * FROM Producto";
+
+  ?>
---
+++ Productos/IndexP.php
@@ -0,0 +1,11 @@
+<?php
+	
+	include("ConsultaP.php");
+	include("HeaderP.html");
+	require_once("DBConnection.php");
+	include("TProductos.php");
+	include("ButtonP.html");
+	include("FooterP.html");
+	
+		
+?>
