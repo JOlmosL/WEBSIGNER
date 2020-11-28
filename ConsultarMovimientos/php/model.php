56eb30b0b570b45a7bb78d6ede058da1d6da8b2a
JesusOlmosLarios <69598051+JesusOlmosLarios@users.noreply.github.com>
2020-11-27 19:56:12 -0600

    Opcion Movimientos Lista

--- ConsultarMovimientos/php/model.php
+++ ConsultarMovimientos/php/model.php
@@ -32,6 +32,7 @@ function select($name, $tabla="ALMACEN", $id="IdAlmacen", $Nombre="NombreAlmacen
     {    
         $resultado .= '<option value="'.$row[$id].'">'.$row[$Nombre].'</option>';
     }
+    $resultado .= '<option value="Todos">Todos</option>';
     
     mysqli_free_result($resultados_consulta); //Liberar la memoria
     
@@ -82,8 +83,12 @@ function realizarConsulta($almacen, $fechai, $fechaf)
     $conexion_bd = conectar();
     $consulta = 'SELECT PERSONAL.NombrePersonal, ALMACEN.NombreAlmacen, STOCK.IdProducto, STOCK.IdStock, MOVIMIENTO.Tipo, MOVIMIENTO.Cantidad, MOVIMIENTO.Destinatario, MOVIMIENTO.Fecha';
     $consulta .= ' FROM PERSONAL NATURAL JOIN MOVIMIENTO NATURAL JOIN ALMACEN NATURAL JOIN STOCK';
-    $consulta .= ' WHERE MOVIMIENTO.IdAlmacen = '.$almacen.' AND MOVIMIENTO.Fecha >= "'.$fechai.'" AND MOVIMIENTO.Fecha <= "'.$fechaf.'"';
-    $consulta .= ' ORDER BY MOVIMIENTO.Fecha DESC';
+
+    if ($almacen=="Todos")
+        $consulta .= ' WHERE MOVIMIENTO.Fecha >= "'.$fechai.'" AND MOVIMIENTO.Fecha <= "'.$fechaf.'"';
+    else
+        $consulta .= ' WHERE MOVIMIENTO.IdAlmacen = '.$almacen.' AND MOVIMIENTO.Fecha >= "'.$fechai.'" AND MOVIMIENTO.Fecha <= "'.$fechaf.'"';
+        $consulta .= ' ORDER BY MOVIMIENTO.Fecha DESC';
 
     $conexion_bd = conectar();
     $resultados_consulta = $conexion_bd->query($consulta);  
@@ -111,6 +116,53 @@ function realizarConsulta($almacen, $fechai, $fechaf)
     
     desconectar($conexion_bd);
     return $resultado;
+}
+
+function PasaraExcel($almacen, $fechai, $fechaf)
+{
+    function cleanData(&$str)
+    {
+        if($str == 't') $str = 'TRUE';
+        if($str == 'f') $str = 'FALSE';
+        if(preg_match("/^0/", $str) || preg_match("/^\+?\d{8,}$/", $str) || preg_match("/^\d{4}.\d{1,2}.\d{1,2}/", $str)) 
+        {
+            $str = "'$str";
+        }
+        if(strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
+        $str = mb_convert_encoding($str, 'UTF-16LE', 'UTF-8');
+    }
+
+    // filename for download
+    $file = "Revision_".date('Ymd').".xls";
+  
+    header("Content-Type: application/vnd.ms-excel; charset=UTF-16LE;");
+    header("Content-Disposition: attachment; filename=$file");
+    
+    $flag = false;
+    $conexion_bd = conectar();
+    $consulta = 'SELECT PERSONAL.NombrePersonal, ALMACEN.NombreAlmacen, STOCK.IdProducto, STOCK.IdStock, MOVIMIENTO.Tipo, MOVIMIENTO.Cantidad, MOVIMIENTO.Destinatario, MOVIMIENTO.Fecha';
+    $consulta .= ' FROM PERSONAL NATURAL JOIN MOVIMIENTO NATURAL JOIN ALMACEN NATURAL JOIN STOCK';
+
+    if ($almacen=="Todos")
+        $consulta .= ' WHERE MOVIMIENTO.Fecha >= "'.$fechai.'" AND MOVIMIENTO.Fecha <= "'.$fechaf.'"';
+    else
+        $consulta .= ' WHERE MOVIMIENTO.IdAlmacen = '.$almacen.' AND MOVIMIENTO.Fecha >= "'.$fechai.'" AND MOVIMIENTO.Fecha <= "'.$fechaf.'"';
+        $consulta .= ' ORDER BY MOVIMIENTO.Fecha DESC';
+   
+    $resultados_consulta = $conexion_bd->query($consulta) or die('¡Consulta fallida!');
+    while($row = mysqli_fetch_array($resultados_consulta, MYSQLI_ASSOC))
+    {
+      if(!$flag) 
+      {
+        // display field/column names as first row
+        echo implode("\t", array_keys($row)) . "\r\n";
+        $flag = true;
+      }
+      array_walk($row, __NAMESPACE__ . '\cleanData');
+      echo implode("\t", array_values($row)) . "\r\n";
+    }
+    desconectar($conexion_bd);
+    exit;
 }
 
 function limpiar_entradas() {
@@ -142,6 +194,5 @@ function limpiar_entradas() {
        $_POST["fechaFConsulta"] = htmlspecialchars($_POST["fechaFConsulta"]);
     }   
 }
-//acusa(5,6);
-//echo tabla_movimientos();
+
 ?>
