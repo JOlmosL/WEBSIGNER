08bd5a2aa20c4f8a0d380129b362b2e34cbae7df
Royd117 <A01273078@itesm.mx>
2020-12-04 08:47:45 -0600

    Better images on product

--- Productos/TProductos.php
+++ Productos/TProductos.php
@@ -3,8 +3,8 @@
 	$result = mysqli_query($conn, $TodosProductos);
 	$content='<div class="row" id="myUL">';
 	while($row = mysqli_fetch_assoc($result)){
-		$content.='<div id="'.$row["IdProducto"].'"class="col-4 card" style="width: 18rem; margin-top: 10px; cursor: pointer;">
-          <a href="../Productos/Cantidad.php?id='.$row["IdProducto"].'"><img class="img-fluid" src="./image/'.$row["IdProducto"].'.png" class="card-img-top imgsetup " alt="..."></a>
+		$content.='<div id="'.$row["IdProducto"].'"class="col-4 card" style="width: 18rem; cursor: pointer;">
+          <a href="../Productos/Cantidad.php?id='.$row["IdProducto"].'"><img style="padding-top: 10px;" class="img-fluid" src="./image/'.$row["IdProducto"].'.png" class="card-img-top imgsetup " alt="..."></a>
           <div class="card-body" style="text-align: center;">
             <label class="card-text">'.$row["NombreProducto"].'</label>
           </div>
